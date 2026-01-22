b117b3a06595172fdce401c783bb705a7a73c3a9
Ibrahim Meshmeshan <ibrahimmeshmeshan@gmail.com>
2026-01-22 15:52:06 +0300

    _ implement register unit test for useCase and repo and model folder

--- lib/core/route/routes_names.dart
+++ lib/core/route/routes_names.dart
@@ -1,8 +1,8 @@
 class RoutesNames {
   static const String splashScreen = '/splash_screen';
   static const String login = '/login';
-  static const String register = '/';//register
-  static const String forgetPassword = '/forgetPassword';
+  static const String register = '/register';
+  static const String forgetPassword = '/';//forgetPassword
   static const String home = '/home';
   static const String auth = '/auth';
 }
